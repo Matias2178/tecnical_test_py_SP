c4c07eb3ab530ab9b867cc939d3c9180a94a61a7
Matias Martelossi <matiasmartelossi@hotmail.com>
2022-09-16 07:25:36 -0300

    Repetition word exercice

---
+++ repetition_word.py
@@ -0,0 +1,37 @@
+def repetition_word(text: str()):
+    """
+    Sumary:
+        Display the number of repetitions of each word from the input text
+
+    Args:
+        text (str): input text
+    
+    Returns:
+        repeated_word 
+    """
+    #clean special characters 
+    especial_character = "|°¬!#$%&/()='?\\¿¡+*{}[]^`,;.:-_<>"
+    
+    for i in especial_character:
+        text = text.replace(i,"")
+        
+    #make the text in lowercase
+    text = text.lower()
+
+    #make a list word
+    word_list = text.split()
+
+    word_dic = {}
+    
+    for i in word_list:
+        if i in word_dic:
+            word_dic[i] +=1
+        else:
+            word_dic[i] = 1
+    for clave, valor in word_dic.items():
+        print(clave,valor)
+        
+
+
+    
+repetition_word("Hi!| ° ¬ how, are. \n \ things? How are you? Are you a developer? I am also a developer")
